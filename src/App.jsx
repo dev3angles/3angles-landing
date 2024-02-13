88349626e913516a2f45deb4ceb5d1c5331fc88f
AVillarreta <andres.villarreta@k2con.com>
2024-02-13 16:29:10 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import "./App.css";
+import Header from "./components/header/header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <img
+        src="./src/assets/triangle.svg"
+        alt="triangle"
+        className="absolute right-0 top-0 no-select"
+        style={{ userSelect: "none", zIndex: "-1" }}
+      />
+
+      <main className="w-full box-border text-white px-10 pt-20 relative z-10">
+        <h1 className="pt-28 text-4xl" style={{ fontFamily: "Moul" }}>
+          Redefiniendo la experiencia <br /> con estilo.
+        </h1>
+        <p className="mt-5">
+          Diseño innovador, análisis preciso de datos y desarrollo sólido <br />
+          para una experiencia digital renovada y auténtica.
+        </p>
+        <div className="flex flex-row gap-4 content-center mt-8">
+          <a href="https://github.com">
+            <img src="./src/assets/github.svg" alt="github" />
+          </a>
+          <a href="#">
+            <img
+              src="./src/assets/phone.svg"
+              alt="phone"
+              style={{ paddingTop: "4px" }}
+            />
+          </a>
+          <a href="#">
+            <img src="./src/assets/mail.svg" alt="mail" />
+          </a>
+          <a href="#">
+            <img
+              src="./src/assets/linkedin.svg"
+              alt="linkedin"
+              style={{ paddingBottom: "5px", paddingLeft: "7px" }}
+            />
+          </a>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header/header.jsx
@@ -0,0 +1,25 @@
+export default function Header() {
+  return (
+    <header
+      style={{ height: "81px" }}
+      className="w-full flex justify-between items-center flex-row box-border px-10 text-white backdrop-blur-sm fixed z-50"
+    >
+      <img
+        srcSet="./src/assets/LOGO_BLACK.svg"
+        alt="LOGO"
+        className="select-none"
+      />
+      <ul className="flex flex-row gap-3">
+        <li className="hover:text-teal-400 transition-all duration-200 cursor-pointer">
+          <a href="#">INICIO</a>
+        </li>
+        <li className="hover:text-teal-400 transition-all duration-200 cursor-pointer">
+          <a href="#">ACERCA DE 3ANGLES</a>
+        </li>
+        <li className="hover:text-teal-400 transition-all duration-200 cursor-pointer">
+          <a href="#">CONTACTANOS</a>
+        </li>
+      </ul>
+    </header>
+  );
+}
